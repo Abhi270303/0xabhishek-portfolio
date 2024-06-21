6837e9447d55ac45b05e11315da0059c7dd7b9c6
Abhishek yadav <barrelabhi45@gmail.com>
2024-06-21 13:01:55 +0530

    Portfolio Completed

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experiences", link: "#experiences" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I specialize in creating secure smart contracts and fostering community engagement in the Web3 space",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development & Community",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building Telegram Mini Apps ",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "LendChips",
+    des: "First cross‑chain lending and borrowing platform for different token standards and NFTs for EVM Compatible Chains",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://devfolio.co/projects/lendchips-f666",
+  },
+  {
+    id: 2,
+    title: "NFT Scout",
+    des: "NFT Scout is an innovative NFT indexer app designed to provide fast and efficient NFT indexing.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "https://nft-scout.vercel.app/", // Updated link
+  },
+  {
+    id: 3,
+    title: "Frictionless DEX",
+    des: " Frictionless DEX pioneers gasless token swaps for a hassle‑free trading experience, accommodating EOA and ERC4337 wallets",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://devfolio.co/projects/frictionless-dex-c827", 
+  },
+  {
+    id: 4,
+    title: "Relief DAO",
+    des: "ReliefDAO is a revolutionary blockchain-driven financial system designed to provide secure and accessible fund transfers to conflict-ridden regions and areas facing crises",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/TechieeGeeeks/Technovate", 
+  },
+];
+
+
+/*export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];*/
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Developer Relations",
+    desc: ": Successfully Conducted Workshops and Gave Tech talks in 15+ Colleges, 6+ Cities in Central India and onboarded 1000+ Enthusiastic users for Qoneqt app. Onboarded 8 Regional Leads for 8 Different regions of India, and currently manage Regional lead Program.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Junior Smart Conntract Dev",
+    desc: "Designed and developed Token contracts in Solidity for Sigil Finance.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 3,
+    title: "Developer Advocate",
+    desc: "Conducted 6 workshops, empowering 1000+ learners with practical knowledge of smart contracts.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Developer Ambassador",
+    desc: "Organised and Managed EtherNitewith Huge success, best side event at EthMumbai held on 28th March at House of Pandora, sponsored and Collaborated by Arbitrum, The Graph and Chainlink.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/Abhi270303",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://twitter.com/0xabhii",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/abhishek-yadav-a567a3250/",
+  },
+];
